72dd58c5f3cfd544cdf263771c72e6b2b3297071
Daniel Neis Araujo <danielneis@gmail.com>
2015-07-29 13:12:16 -0300

    MDL-22078 course: add end date

--- lib/db/upgrade.php
+++ lib/db/upgrade.php
@@ -4440,5 +4440,19 @@ function xmldb_main_upgrade($oldversion) {
         upgrade_main_savepoint(true, 2015051100.10);
     }
 
+    if ($oldversion < 2015051101.01) {
+
+        // Define field enddate to be added to course.
+        $table = new xmldb_table('course');
+        $field = new xmldb_field('enddate', XMLDB_TYPE_INTEGER, '10', null, null, null, 0, 'startdate');
+
+        // Conditionally launch add field enddate.
+        if (!$dbman->field_exists($table, $field)) {
+            $dbman->add_field($table, $field);
+        }
+
+        upgrade_main_savepoint(true, 2015051101.01);
+    }
+
     return true;
 }
