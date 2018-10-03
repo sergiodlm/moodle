364a4ec147f6ab578257368703de5da0635e72ff
Toni Barberà Melià <toni@moodle.com>
2018-10-03 10:39:18 +0200

    global search results display format

--- customfield/classes/search/course_customfields.php
+++ customfield/classes/search/course_customfields.php
@@ -93,13 +93,14 @@ class course_customfields extends \core_search\base_mod {
             return false;
         }
 
-        // Get the non-null $record(Data) value. Only 1 must be !null
+        // Getting the non-null $record(Data) value. Only 1 must be !null
         $datavalue = (string) ($record->intvalue ??
                                $record->decvalue ??
                                $record->shortcharvalue ??
                                $record->charvalue ??
                                $record->value);
 
+        // Getting the couse info for results view
         if ($modinfo = get_fast_modinfo($record->recordid)) {
             $course = $modinfo->get_course();
         } else {
@@ -111,7 +112,10 @@ class course_customfields extends \core_search\base_mod {
         // Prepare associative array with data from DB.
         $doc = \core_search\document_factory::instance($record->id, $this->componentname, $this->areaname);
         $doc->set('title', content_to_text($course->fullname, false));
-        $doc->set('content', content_to_text("{$field->get('name')}: $datavalue", \core_search\manager::TYPE_TEXT));
+        $doc->set('content', content_to_text(
+                "{$field->get('name')}: $datavalue",
+                \core_search\manager::TYPE_TEXT)
+        );
         $doc->set('contextid', $context->id);
         $doc->set('courseid', $record->recordid);
         $doc->set('owneruserid', \core_search\manager::NO_OWNER_ID);
