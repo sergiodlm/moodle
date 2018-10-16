46888a41d2faab27cac0f07f684163ab47426ca5
Toni Barberà Melià <toni@moodle.com>
2018-10-16 10:48:26 +0200

    unit tests

--- customfield/tests/category_test.php
+++ customfield/tests/category_test.php
@@ -26,6 +26,8 @@
 namespace core_customfield;
 
 use advanced_testcase;
+use Horde\Socket\Client\Exception;
+use Phpml\Exception\DatasetException;
 use stdClass;
 
 defined('MOODLE_INTERNAL') || die();
@@ -44,7 +46,7 @@ class core_customfield_category_testcase extends advanced_testcase {
     /**
      * @throws \moodle_exception
      */
-    public function test_create_category() {
+    public function test_create_category_and_correctlly_reorder() {
 
         // Create the category.
         $categorydata            = new stdClass();
@@ -120,12 +122,176 @@ class core_customfield_category_testcase extends advanced_testcase {
         $this->assertSame((int) $category1->get('sortorder'), 1);
         $this->assertSame((int) $category2->get('sortorder'), 2);
 
+        // Creating 4th category and check if sortorder is correct.
+        $categorydata->name = 'dddd';
+
+        $category3 = new category(0, $categorydata);
+        $category3->save();
+
+        // Initially confirm that base data was inserted correctly.
+        $this->assertSame($category3->get('name'), $categorydata->name);
+        $this->assertSame($category3->get('description'), null);
+        $this->assertSame($category3->get('descriptionformat'), '0');
+        $this->assertSame($category3->get('component'), $categorydata->component);
+        $this->assertSame($category3->get('area'), $categorydata->area);
+        $this->assertSame($category3->get('itemid'), $categorydata->itemid);
+        $this->assertSame($category3->get('contextid'), $categorydata->contextid);
+        $this->assertSame($category3->get('sortorder'), -1);
+
+        $id3 = $category3->get('id');
+
+        // Check order after re-fetch.
+        $category0 = new category($id0);
+        $category1 = new category($id1);
+        $category2 = new category($id2);
+        $category3 = new category($id3);
+
+        $this->assertSame((int) $category0->get('sortorder'), 0);
+        $this->assertSame((int) $category1->get('sortorder'), 1);
+        $this->assertSame((int) $category2->get('sortorder'), 2);
+        $this->assertSame((int) $category3->get('sortorder'), 3);
     }
 
-    // TODO: Exceptions tests
-    // TODO: Update name tests
+    /**
+     * @throws \moodle_exception
+     */
+    public function test_create_category_and_rename() {
+        // Create the category.
+        $categorydata            = new stdClass();
+        $categorydata->name      = 'aaaa';
+        $categorydata->component = 'core_course';
+        $categorydata->area      = 'course';
+        $categorydata->itemid    = 0;
+        $categorydata->contextid = 1;
+
+        $category0 = new category(0, $categorydata);
+        $category0->save();
+
+        // Initially confirm that base data was inserted correctly.
+        $this->assertSame($category0->get('name'), $categorydata->name);
+        $this->assertSame($category0->get('description'), null);
+        $this->assertSame($category0->get('descriptionformat'), '0');
+        $this->assertSame($category0->get('component'), $categorydata->component);
+        $this->assertSame($category0->get('area'), $categorydata->area);
+        $this->assertSame($category0->get('itemid'), $categorydata->itemid);
+        $this->assertSame($category0->get('contextid'), $categorydata->contextid);
+        $this->assertSame($category0->get('sortorder'), -1);
+
+        // Checking new name are correct updated.
+        $newname = 'bbbb';
+        $category0->set('name', $newname);
+        $this->assertSame($category0->get('name'), $newname);
+
+
+        // Checking new name are correct updated after save.
+        $category0->save();
+        $id = $category0->get('id');
+
+        $category0 = new category($id);
+        $this->assertSame($category0->get('name'), $newname);
+    }
+
+    /**
+     * @throws \moodle_exception
+     */
+    public function test_create_category_and_delete() {
+        // Create the category.
+        $categorydata            = new stdClass();
+        $categorydata->name      = 'aaaa';
+        $categorydata->component = 'core_course';
+        $categorydata->area      = 'course';
+        $categorydata->itemid    = 0;
+        $categorydata->contextid = 1;
+
+        $category0 = new category(0, $categorydata);
+        $category0->save();
+
+        $id = $category0->get('id');
+
+        // Confirm that exist in the database.
+        $this->assertTrue(category::record_exists($id));
+
+        //Delete and confirm that is deleted.
+        $category0->delete();
+        $this->assertFalse(category::record_exists($id));
+    }
+
     // TODO: Drag adn drop tests
+    /**
+     * @throws \coding_exception
+     */
+    public function test_create_categories_and_move_it_as_drag_and_drop() {
+        // Create the categories.
+        $categorydata            = new stdClass();
+        $categorydata->name      = 'aaaa';
+        $categorydata->component = 'core_course';
+        $categorydata->area      = 'course';
+        $categorydata->itemid    = 0;
+        $categorydata->contextid = 1;
+        $category0               = new category(0, $categorydata);
+        $category0->save();
+        $id0 = $category0->get('id');
+
+        $categorydata->name      = 'bbbb';
+        $category1               = new category(0, $categorydata);
+        $category1->save();
+        $id1 = $category1->get('id');
+
+        $categorydata->name      = 'cccc';
+        $category2               = new category(0, $categorydata);
+        $category2->save();
+        $id2 = $category2->get('id');
+
+        $categorydata->name      = 'dddd';
+        $category3               = new category(0, $categorydata);
+        $category3->save();
+        $id3 = $category3->get('id');
+
+        $categorydata->name      = 'eeee';
+        $category4               = new category(0, $categorydata);
+        $category4->save();
+        $id4 = $category4->get('id');
+
+        $categorydata->name      = 'ffff';
+        $category5               = new category(0, $categorydata);
+        $category5->save();
+        $id5 = $category5->get('id');
+
+        // Check order after re-fetch.
+        $category0 = new category($id0);
+        $category1 = new category($id1);
+        $category2 = new category($id2);
+        $category3 = new category($id3);
+        $category4 = new category($id4);
+        $category5 = new category($id5);
+
+        $this->assertSame((int) $category0->get('sortorder'), 0);
+        $this->assertSame((int) $category1->get('sortorder'), 1);
+        $this->assertSame((int) $category2->get('sortorder'), 2);
+        $this->assertSame((int) $category3->get('sortorder'), 3);
+        $this->assertSame((int) $category4->get('sortorder'), 4);
+        $this->assertSame((int) $category5->get('sortorder'), 5);
+
+        // Move up 1 position.
+        // Move down 1 position.
+
+        // Move up 2 positions.
+        // Move down 2 positions.
+
+        // Move up 3 positions.
+        // Move down 3 positions.
+
+        //Move to the beginning of the list.
+        //Move to the end of the list.
+    }
+
+
+
+
+    // TODO: Exceptions tests
+
+
     // TODO: Visibility tests
-    // TODO: Delete tests
+
 
 }
