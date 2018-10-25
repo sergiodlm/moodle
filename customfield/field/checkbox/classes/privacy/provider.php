353299be3cd5a5de9bc1f701e7146bb3e4e5cfe1
Daniel Neis Araujo <danielneis@gmail.com>
2018-10-25 11:39:37 -0300

    adding privacy providers to customfield standard plugins

---
+++ customfield/field/checkbox/classes/privacy/provider.php
@@ -0,0 +1,71 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * Privacy Subsystem implementation for customfield_checkbox.
+ *
+ * @package    customfield_checkbox
+ * @copyright  2018 Daniel Neis Araujo <danielneis@gmail.com>
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+namespace customfield_checkbox\privacy;
+
+use core_privacy\local\request\transform;
+use core_privacy\local\request\writer;
+
+defined('MOODLE_INTERNAL') || die();
+
+/**
+ * Privacy Subsystem for customfield_checkbox implementing null_provider.
+ *
+ * @copyright  2018 Daniel Neis Araujo <danielneis@gmail.com>
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+class provider extends \core_customfield\privacy\provider implements \core_privacy\local\metadata\null_provider {
+
+    /**
+     * Get the language string identifier with the component's language
+     * file to explain why this plugin stores no data.
+     *
+     * @return  string
+     */
+    public static function get_reason() : string {
+        return 'privacy:metadata';
+    }
+
+    /**
+     * Exports data about one record in {customfield_data} table.
+     *
+     * @param \context_module $context
+     * @param \stdClass $recordobj record from DB table {data_records}
+     * @param \stdClass $fieldobj record from DB table {data_fields}
+     * @param \stdClass $contentobj record from DB table {data_content}
+     * @param \stdClass $defaultvalue pre-populated default value that most of plugins will use
+     */
+    public static function export_customfield_data($context, $fieldobj, $dataobj) {
+        writer::with_context($context)->export_data([$fieldobj->id, $dataobj->id], $dataobj);
+    }
+
+    /**
+     * Allows plugins to delete locally stored data.
+     *
+     * @param \context_module $context
+     * @param \stdClass $fieldobj record from DB table {customfield_field}
+     * @param \stdClass $dataobj record from DB table {customfield_data}
+     */
+    public static function delete_data_content($context, $fieldobj, $dataobj) {
+    }
+}
--- customfield/field/checkbox/lang/en/customfield_checkbox.php
+++ customfield/field/checkbox/lang/en/customfield_checkbox.php
@@ -24,4 +24,18 @@ defined('MOODLE_INTERNAL') || die();
 
 $string['checkedbydefault'] = 'Checked by default';
 $string['pluginname'] = 'Checkbox';
+$string['privacy:metadata:customfield_data'] = 'Represents custom field data saved to a context';
+$string['privacy:metadata:customfield_data:fieldid'] = 'Field definition ID';
+$string['privacy:metadata:customfield_data:instanceid'] = 'Instance ID related to the data';
+$string['privacy:metadata:customfield_data:intvalue'] = 'Data value, when it is an integer';
+$string['privacy:metadata:customfield_data:decvalue'] = 'Data value, when it is a decimal ';
+$string['privacy:metadata:customfield_data:shortcharvalue'] = 'Data value, when it is a short char';
+$string['privacy:metadata:customfield_data:charvalue'] = 'Data value, when it is a char';
+$string['privacy:metadata:customfield_data:value'] = 'Data value, when it is a text';
+$string['privacy:metadata:customfield_data:valueformat'] = 'The format of the value, when it is a text';
+$string['privacy:metadata:customfield_data:timecreated'] = 'Time when data was created';
+$string['privacy:metadata:customfield_data:timemodified'] = 'Time when data was last modified';
+$string['privacy:metadata:customfield_data:contextid'] = 'The ID of the context where the data was saved';
+$string['privacy:metadata:customfieldpluginsummary'] = 'Fields for various components';
+$string['privacy:metadata:filepurpose'] = 'File attached to the custom field data';
 $string['specificsettings'] = 'Settings for the checkbox field';
